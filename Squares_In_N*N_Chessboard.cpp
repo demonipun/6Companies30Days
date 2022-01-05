869f8eae6d2dc9bf59ab9b8813d9694c5b66371c
Nipun <71482945+demonipun@users.noreply.github.com>
2022-01-05 13:11:29 +0530

    Create Squares_In_N*N_Chessboard.cpp

---
+++ Squares_In_N*N_Chessboard.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    
+    // https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1
+    
+    long long squaresInChessBoard(long long n) {
+        // Number of 1*1 squares = n*n
+        // Number of 2*2 squares = (n-1)*(n-1)
+        // ...
+        // Number of n*n squares = 1*!
+        // Total squares = Summation of(1*1+2*2+ ... +n*n)
+        return n*(n+1)*(2*n+1)/6;
+    }
+};
