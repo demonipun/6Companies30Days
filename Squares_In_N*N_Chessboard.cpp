3dd1a1641c78624f8bb257e0f7dd3eb5d46fd571
Nipun <71482945+demonipun@users.noreply.github.com>
2022-01-08 06:13:54 +0530

    Update Squares_In_N*N_Chessboard.cpp

--- Squares_In_N*N_Chessboard.cpp
+++ Squares_In_N*N_Chessboard.cpp
@@ -7,8 +7,8 @@ class Solution {
         // Number of 1*1 squares = n*n
         // Number of 2*2 squares = (n-1)*(n-1)
         // ...
-        // Number of n*n squares = 1*!
-        // Total squares = Summation of(1*1+2*2+ ... +n*n)
+        // Number of n*n squares = 1*1
+        // Total squares = 1*1+2*2+ ... +n*n
         return n*(n+1)*(2*n+1)/6;
     }
 };
